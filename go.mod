201ecd0ce6b6403eeee15a07cfc493bd517d8c74
Boran Car <boran.car@gmail.com>
2021-05-23 17:14:02 +0300

    Initial set-up
    Set the provider up.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/borancar/terraform-provider-talos
+
+go 1.15
+
+require github.com/hashicorp/terraform-plugin-sdk/v2 v2.6.1
---
+++ talos/provider.go
@@ -0,0 +1,11 @@
+package talos
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		ResourcesMap: map[string]*schema.Resource{},
+	}
+}
